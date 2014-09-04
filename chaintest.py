b1510797f4e003b1dd9cc8d58cc9448bb05343ef
samuel albanie <satiricallaughter@gmail.com>
2014-09-04 09:37:48 +0100

    added differ_by_one test

--- chaintest.py
+++ chaintest.py
@@ -1,10 +1,19 @@
 import unittest
 import chains
 
-with open('/usr/share/dict/words', 'r') as word_file:
-    english_dict = {line.rstrip().lower() for line in word_file}
 
 class ChainTest(unittest.TestCase):
+    def assert_differ_by_one(self, word, next):
+        character_pairs = zip(word, next)
+        def not_equal_chars(pair):
+            return pair[0] != pair[1]
+        filtered_pairs = list(filter(not_equal_chars, character_pairs))
+        self.assertEqual(
+            len(filtered_pairs),
+            1,
+            "filter_pairs: {0}".format(filtered_pairs),
+        )
+
     def test_check_words_in_english(self):
         start_word = 'cat'
         end_word = 'dog'
@@ -12,10 +21,17 @@ class ChainTest(unittest.TestCase):
 
         for word in words:
             with self.subTest(word=word):
-                self.assertIn(word, english_dict)
+                self.assertIn(word, chains.english_dict)
 
     def test_words_differ_by_one_letter(self):
-        pass
+        start_word = 'cat'
+        end_word = 'dog'
+        words = chains.find_chain(start_word, end_word)
+
+        word_pairs = zip(words, words[1:])
+        for word, next in word_pairs:
+            with self.subTest(word=word, next=next):
+                self.assert_differ_by_one(word, next)
 
 if __name__ == "__main__":
     unittest.main()
